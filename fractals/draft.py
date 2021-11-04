a559ad420b61fa12c6745b3fec90c14c5939334c
2, 8, 20, 28, 50, 82, 126 <53151326+Reklle@users.noreply.github.com>
2021-11-04 04:32:16 +0300

    v 2.2
    better render

--- fractals/draft.py
+++ fractals/draft.py
@@ -17,12 +17,23 @@ DRAFTCOLOR = np.array([0, 0, 0, 1])
 GRASSCOLOR = np.array([0.58, 0.65, 0.35, 1])
 
 
+def prerender(s):
+    """return recommended scale for plot"""
+    height = 0
+    bi = 0  # initial bracket depth
+    m = tools.bracket_abyss(s)  # maximal bracket depth
+    s = tools.debracket(s)
+    h = len(s)
+    return HEIGHT / h * 0.9
+
+
 def draft_render(s, ctx, scale=1, dangle=tools.radians(20), alpha=1, x0=WIDTH / 2, y0=HEIGHT / 2):
     """ Hint:
     F - draw line
     +   just clockwise rotation
     -   just anticlockwise rotation
     r   random rotation
+    s   alternating rotation
     []  brackets
     ---------------
     dangle -- delta angle (rotation)
@@ -31,25 +42,21 @@ def draft_render(s, ctx, scale=1, dangle=tools.radians(20), alpha=1, x0=WIDTH / 
     bi = 0  # initial bracket depth
     m = tools.bracket_abyss(s)  # maximal bracket depth
     angle = []  # initial values for any bracket depth
-    x = []  #
-    y = []  #
-
+    x = []
+    y = []
+    a = []  # alternating index
     for i in range(0, tools.bracket_abyss(s)):
         angle.append(0)
         x.append(0)
         y.append(0)
+        a.append(0)
 
     x[0] = x0  # initial coordinates
     y[0] = y0  #
-
     dp = 1 / len(s)
     p = 0
     dp_step = 0.1
     for c in s:
-        p += dp
-        if (p > dp_step):
-            dp_step += 0.1
-            # print(p)
 
         if (c == "F"):
             # new coordinates
@@ -69,10 +76,17 @@ def draft_render(s, ctx, scale=1, dangle=tools.radians(20), alpha=1, x0=WIDTH / 
             angle[bi] += dangle
 
         if (c == "-"):
-            angle[bi] += dangle
+            angle[bi] -= dangle
 
         if (c == "r"):
             if (random.randint(0, 1) == 0):
+                angle[bi] += dangle
+            else:
+                angle[bi] -= dangle
+
+        if (c == "s"):
+            a[bi] += 1
+            if (a[bi] % 2 == 0):
                 angle[bi] += dangle
             else:
                 angle[bi] -= dangle
@@ -87,15 +101,8 @@ def draft_render(s, ctx, scale=1, dangle=tools.radians(20), alpha=1, x0=WIDTH / 
             bi -= 1  # bracket depth decreasing
 
 
-def cool_render(s, ctx, length=1, dangle=tools.radians(20), alpha=1, x0=WIDTH / 2, y0=HEIGHT / 2):
-    """ Hint:
-    F - draw line
-    +   just clockwise rotation
-    -   just anticlockwise rotation
-    r   random rotation
-    []  brackets
-    ---------------
-    dangle -- delta angle (rotation)
+def squiggly_render(s, ctx, length=1, dangle=tools.radians(20), alpha=1, x0=WIDTH / 2, y0=HEIGHT / 2):
+    """ the same as draft_render, but draw more wavy
     """
 
     bi = 0  # initial bracket depth
@@ -118,7 +125,7 @@ def cool_render(s, ctx, length=1, dangle=tools.radians(20), alpha=1, x0=WIDTH / 
 
         if (c == "F"):
             # new coordinates
-            angle[bi] += random.gauss(0, 0.02)
+            angle[bi] += random.gauss(0, 0.005)
             new_x = x[bi] + random.gauss(length, sigma_length) * np.sin(angle[bi])
             new_y = y[bi] - random.gauss(length, sigma_length) * np.cos(angle[bi])
             # drawing
@@ -134,10 +141,10 @@ def cool_render(s, ctx, length=1, dangle=tools.radians(20), alpha=1, x0=WIDTH / 
             y[bi] = new_y
 
         if (c == "+"):
-            angle[bi] += random.gauss(dangle, 0.05)
+            angle[bi] += random.gauss(dangle, 0.01)
 
         if (c == "-"):
-            angle[bi] -= random.gauss(dangle, 0.05)
+            angle[bi] -= random.gauss(dangle, 0.01)
 
         if (c == "r"):
             angle[bi] -= random.gauss(0, dangle)
