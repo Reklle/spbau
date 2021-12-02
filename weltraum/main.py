8e69dfd077834a79013a6d213a3fdef74e1c2c42
2, 8, 20, 28, 50, 82, 126 <53151326+Reklle@users.noreply.github.com>
2021-12-02 20:53:51 +0300

    Add files via upload

--- weltraum/main.py
+++ weltraum/main.py
@@ -1,217 +1,194 @@
-from abc import abstractmethod, ABC
-
-import numpy as np
-from numpy import array
-from numpy import longfloat as num
-import numpy as np
+import astropy.units as u
 import matplotlib.pyplot as plt
+import numpy as np
+from einsteinpy.coordinates.conversion import BoyerLindquistConversion as blc
+from einsteinpy.coordinates.conversion import CartesianConversion as cc
+from einsteinpy.geodesic import Geodesic
+from einsteinpy.plotting import ShadowPlotter
+from einsteinpy.rays import Shadow
+from einsteinpy.metric import Kerr
+from einsteinpy.metric import KerrNewman
+from einsteinpy.coordinates import BoyerLindquistDifferential
+from corrupted_einsteinpy.static import StaticGeodesicPlotter
 
-class PlotStyle:
-    do_aspect = 0
-
-    fig, ax = plt.subplots(1)
-
-    def __init__(self, title="", changing_plot_color=0, foreground="#000000", axesground="#000000", plotgroud="#000000",
-                 background="#FFFFFF"):
-        self.title = title
-        self.foreground = foreground
-        self.axesground = axesground
-        self.plotgroud = plotgroud
-        self.background = background
-        self.changing_plot_color = changing_plot_color
-
-    def set_changes(self):
-        self.fig.patch.set_facecolor(self.background)
-        title_obj0 = self.ax[0].set(ylabel="Height", title="Rocket path")
-        title_obj1 = self.ax[1].set(ylabel="Air resistance")
-        title_obj2 = self.ax[2].set(ylabel="Grav. acceleration")
-        self.fig.canvas.manager.set_window_title(self.title)
-
-        for q in range(3):
-            self.ax[q].set_facecolor(self.background)
-            self.ax[q].spines['left'].set_color(self.axesground)
-            self.ax[q].spines['bottom'].set_color(self.axesground)
-            self.ax[q].spines['top'].set_color(self.axesground)
-            self.ax[q].spines['right'].set_color(self.axesground)
-            self.ax[q].tick_params(axis='x', colors=self.foreground)
-            self.ax[q].tick_params(axis='y', colors=self.foreground)
-
-        if (self.do_aspect == 1):
-            self.ax[0].set_aspect(1)
-
-        plt.setp(title_obj0, color=self.foreground)
-        plt.setp(title_obj1, color=self.foreground)
-        plt.setp(title_obj2, color=self.foreground)
-
-    def legend(self):
-        self.ax.legend(fontsize=25, ncol=2, facecolor='oldlace', dgecolor='r', title='Прямые', title_fontsize='20')
-
-class RightRody(ABC):
-    def __init__(self, position, velocity, target = None):
-        if target == None:
-            self.position = position
-            self.velocity = velocity
-        else:
-            self.position = position + target.position
-            self.velocity = velocity + target.velocity
-
-    def step(self, dt):
-        pass
-
-class CelestialBody(RightRody):
-    def __init__(self, radius, φ, dφ, **kwargs):
-        if 'target' in kwargs:
-            position = np.array([np.cos(φ), np.sin(φ)]) * radius
-            velocity = np.array([np.cos(dφ), np.sin(dφ)]) * radius
-            super().__init__(position, velocity, kwargs['target'])
-        else:
-            super().__init__(0, 0)
-
-    @abstractmethod
-    def step(self, dt):
-        self.position += self.velocity*dt
-        ...
-
-class Planet(CelestialBody):
-    def __init__(self, radius, φ, dφ, **kwargs):
-        super().__init__(radius, φ, dφ, **kwargs)
-        self.temperature = kwargs['temperature']
-
-    def step(self, dt):
-        self.position += self.velocity*dt
-
-class Star(CelestialBody):
-    def __init__(self, radius, φ, dφ, **kwargs):
-        super().__init__(radius, φ, dφ, **kwargs)
-
-class BlackHole(CelestialBody):
-    def __init__(self, radius, φ, dφ, **kwargs):
-        super().__init__(radius, φ, dφ, **kwargs)
-
-class Universe(ABC):
-    def __init__(self, G, fow = -2):
-        """G - gravitation constant
-        fow - power in formula of gravitation"""
-        self.G = G
-        self.fow = fow
-        self.objects = []
-
-    @abstractmethod
-    def append(self, obj : CelestialBody):
-        self.objects.append(obj)
-        ...
-
-    @abstractmethod
-    def step(self):
-        """Step of computing"""
-        for o in self.objects:
-            o.step(0.1)
-        ...
-
-    @abstractmethod
-    def positions(self):
-        """Step of computing"""
-        ...
-        for o in self.objects:
-            yield o.position
-
-class ClassicUniverse2D(Universe):
-    def __init__(self, G, fow=-2):
-        super().__init__(G, fow)
+A = 0.5       # rotating parameter
+Q = 0.5       # charge parameter
+M = 2e38    # mass
 
 
-    def step(self):
-        for o in self.objects:
-            o.step(0.1)
-
-    def append(self, obj : CelestialBody):
-        self.objects.append(obj)
-
-    def positions(self):
-        for o in self.objects:
-            yield o.position
-
-def pw(a, p):
-    return num(a) * np.power(10, p, dtype=np.longfloat)
-
-def vec(x, y, z):
-    return array([x, y, z], dtype=np.longfloat)
+def tr(a, b, c, d):
+    """Convert from Boyer Lindquist coordinates to Cartesian"""
+    return blc(a, b, c, d).convert_cartesian(M=M, a=0)
 
 
-SolarMass = pw(4, 37)
-EarthMass = pw(6, 24)
-AU = pw(1.4959, 13)
-def Newtone():
-    G = pw(6.674, -11)
-    MoonMass = pw(7, 22)
-
-    q = vec(AU / 10, 0, 0)
-    r = np.linalg.norm(q)
-    dq = vec(0, np.sqrt(G * SolarMass / r) * 1.2, 0)
-    c = pw(3, 8)
-    mu = SolarMass * EarthMass / (SolarMass + EarthMass)
-    while 1:
-        q += dq * DT
-        r = np.linalg.norm(q)
-        dq -= DT * q * G * SolarMass / r ** 3
-        yield q/AU
-
-        
-
-def Arrpox():
-    G = pw(6.674,-11)
-    MoonMass = pw(7, 22)
+def rt(a, b, c, d):
+    """Convert from Cartesian coordinates to Boyer Lindquist"""
+    # when a != 0 then there is fatal error, but it works right with a = 0
+    return cc(a, b, c, d).convert_bl(M=M, a=0)
 
 
+def show():
+    """Visualize black hole"""
+    bl = BoyerLindquistDifferential(
+        t=0. * u.s,
+        r=1e3 * u.m,
+        theta=np.pi / 2 * u.rad,
+        phi=np.pi * u.rad,
+        v_r=0. * u.m / u.s,
+        v_th=0. * u.rad / u.s,
+        v_p=0. * u.rad / u.s,
+    )
+    kn = KerrNewman(coords=bl, M=M*u.kg, a=A*u.one, Q = Q*u.C, q = 0*u.C/u.kg)
+    sing_dict = kn.singularities()
+    theta = np.linspace(0, 2 * np.pi, 500)
+    Ei, Eo = sing_dict["inner_ergosphere"], sing_dict["outer_ergosphere"]
+    Ei_list, Eo_list = Ei(theta), Eo(theta)
+    Xei = Ei_list * np.sin(theta)
+    Xeo = Eo_list * np.sin(theta)
+    Yei = Ei_list * np.cos(theta)
+    Yeo = Eo_list * np.cos(theta)
+    Hi, Ho = sing_dict["inner_horizon"], sing_dict["outer_horizon"]
+    Xhi = Hi * np.sin(theta)
+    Xho = Ho * np.sin(theta)
+    Yhi = Hi * np.cos(theta)
+    Yho = Ho * np.cos(theta)
 
-    q = vec(AU/10, 0, 0)
-    r = np.linalg.norm(q)
-    dq = vec(0, np.sqrt(G*SolarMass/r)*1.2, 0)
-    c = pw(3, 8)
-    while 1:
-        q+=dq*DT
-        r = np.linalg.norm(q)
-        hh = np.linalg.norm(np.cross(q, dq))**2
-        dq -= DT * q * G*np.exp(1477*1e7/r) * SolarMass / r ** 3
+    fig, ax = plt.subplots(1, 1)
 
-        yield q/AU
+    ax.fill(Xei, Yei, 'b', Xeo, Yeo, 'r', Xhi, Yhi, 'b', Xho, Yho, 'r', alpha=0.3)
+    ax.set_aspect(1)
+    plt.show()
 
-DT = pw(5, 1)
 
-n = Newtone()
-a = Arrpox()
-X, Y = [], []
-x, y = [], []
-for i in range(100000):
-    p = n.__next__()
-    pp = a.__next__()
+def blackhole_intensity():
+    """Plot of intensivity of black hole radiation"""
+    mass = 1e3 * u.kg
+    fov = 4e4 * u.km
+    shadow = Shadow(mass=mass, fov=fov, n_rays=400)
+    obj = ShadowPlotter(shadow=shadow, is_line_plot=True)
+    obj.plot()
+    obj.show()
 
-    X.append(p[0])
-    Y.append(p[1])
-    x.append(pp[0])
-    y.append(pp[1])
 
-st = PlotStyle("")
-st.ax.plot(X, Y)
-st.ax.plot(x, y)
-st.ax.set_aspect(1)
+def stable_geodesic(momentum):
+    """This is the stable orbit near the rotating black hole"""
 
-G = pw(6.674,-11)
-SolarMass = pw(1.988, 30)
-EarthMass = pw(6, 24)
-MoonMass = pw(7, 22)
-c = pw(3, 8)
-AU = pw(1.4959, 11)
+    orbit = 1.0006  # orbit radius in GM/c^2
+    position = rt(0, orbit, 0, 0)[1:]
 
-c = plt.Circle((0, 0), 2*(c/AU), color='y')
-st.ax.add_patch(c)
-plt.show()
+    geod = Geodesic(
+        metric="KerrNewman",
+        metric_params=(A,Q),
+        position=position,
+        momentum=momentum,
+        steps=100,
+        delta=0.00005,
+        return_cartesian=True,
+        order=2,
+        omega=0.01
+    )
 
-# c = ClassicUniverse2D(4)
-# p0 = Planet(0, 0, 1, temperature=1)
-# p1 = Planet(1, 0, 1, target= p0, temperature=1)
-# c.append(p0)
-# c.append(p1)
-# print(list(c.positions()))
-# c.step()
-# print(list(c.positions()))
+    sgpl = StaticGeodesicPlotter()
+    sgpl.draw_ergosphere = 1
+    sgpl.plot2D(geod, (3, 2))
+    sgpl.ax.set_aspect(1)
+    sgpl.draw_ergosphere = 0
+    sgpl.plot2D(geod, (1, 2))
+    sgpl.ax.set_aspect(1)
+    sgpl.show()
+
+    return geod
+
+
+def geodesic(position, momentum):
+    # parameters
+    a = 1
+    M = 2e38
+
+    geod = Geodesic(
+        metric="Kerr",
+        metric_params=(A,Q),
+        position=position,
+        momentum=momentum,
+        steps=100,
+        delta=0.005,
+        return_cartesian=True,
+        order=2,
+        omega=0.01
+    )
+
+    sgpl = StaticGeodesicPlotter()
+    sgpl.draw_ergosphere = 1
+    sgpl.plot2D(geod, (3, 2))
+    sgpl.ax.set_aspect(1)
+    sgpl.show()
+    sgpl.draw_ergosphere = 0
+    sgpl.plot2D(geod, (1, 2))
+    sgpl.ax.set_aspect(1)
+    sgpl.show()
+
+    return geod
+
+
+def kepler(trajectory):
+    def derivative(array, dt):
+        ret = []
+        for i in range(len(array) - 1):
+            ret.append((array[i + 1] - array[i]) / dt)
+        return ret
+
+    def triangle(x, y, z, dx, dy, dz):
+        i = np.prod([y, dz], axis=0) - np.prod([z, dy], axis=0)
+        j = np.prod([z, dx], axis=0) - np.prod([x, dz], axis=0)
+        k = np.prod([x, dy], axis=0) - np.prod([y, dx], axis=0)
+
+        return np.sqrt(np.square(i) + np.square(j) + np.square(k))
+
+    t = trajectory[:, 0]
+    # dt is almost constant
+    dt = trajectory[0, 1] - trajectory[0, 0]
+
+    x, y, z = trajectory[:, 1], trajectory[:, 2], trajectory[:, 3]
+    dx, dy, dz = derivative(x, dt), derivative(y, dt), derivative(z, dt)
+
+    ds = triangle(x[1:], y[1:], z[1:], dx, dy, dz)
+    axis = range(len(ds))
+
+    plt.plot(axis, ds)
+    plt.show()
+
+
+if __name__ == "__main__":
+    n = -1
+
+    if n == -1:
+        blackhole_intensity()
+        show()
+
+    elif n == 0:
+        # there motion is almost not depend on momentum !
+        momentum = [0, 0, 0]
+        geod = stable_geodesic(momentum)
+        # second kepler's rule works!
+        kepler(geod._trajectory[1])
+
+    elif n == 1:
+        position = rt(0, 2, 0, 0)[1:]
+        momentum = [0, 0, 3.5]
+        geod = geodesic(position, momentum)
+        # second kepler's rule doesn't works
+        kepler(geod._trajectory[1])
+
+    elif n == 2:
+        position = rt(0, 0.7, 1.2, 1.2)[1:]
+        momentum = [0, 0, 0]
+        geod = geodesic(position, momentum)
+        # second kepler's rule doesn't works
+        kepler(geod._trajectory[1])
+
+    elif n == 3:
+        position = rt(0, 0.4, 1.2, 1.2)[1:]
+        momentum = [0, 0, 3.5]
+        geod = geodesic(position, momentum)
+        # second kepler's rule doesn't works
+        kepler(geod._trajectory[1])
