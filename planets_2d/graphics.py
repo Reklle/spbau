17354ead70985681ba91ed88b5905f1be1e28751
2, 8, 20, 28, 50, 82, 126 <53151326+Reklle@users.noreply.github.com>
2021-12-06 04:56:53 +0300

    v 1.8

---
+++ planets_2d/graphics.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+plt.style.use(['dark_background', 'seaborn-notebook'])  # you can also use 'bmh'
+
+fig = plt.figure()
+
+fig.subplots_adjust(left=0, bottom=0, right=1, top=1, wspace=0, hspace=0)
+ax = plt.axes(xlim=(-100, 100), ylim=(-100, 100))
+ax.set_aspect('equal')
+fig.subplots_adjust(left=0, bottom=0, right=1, top=1, wspace=0, hspace=0)
+ax.spines['left'].set_color('#3C403D')
+ax.spines['bottom'].set_color('#3C403D')
+ax.spines['top'].set_color('#3C403D')
+ax.spines['right'].set_color('#3C403D')
+ax.tick_params(axis='x', colors='#3C403D')
+ax.tick_params(axis='y', colors='#3C403D')
---
+++ planets_2d/main.py
@@ -0,0 +1,175 @@
+import copy
+
+import numpy as np
+
+from graphics import *
+
+TAIL = 20  # length of rendering tail
+
+
+class Simulation:
+    def __init__(self, **kwargs):
+        self.dt = kwargs.get("dt", 0.005)
+        self.pause = False
+
+
+simulation = Simulation()
+
+
+class CelestialBody:
+    """Parent class for universe objects"""
+
+    def __init__(self, radius, φ, dr, **kwargs):
+        self.name = kwargs.get("name", "")
+        self.texture_id = kwargs.get("texture", -1)
+        self.radius = kwargs.get("radius", -1)
+        self.rotation = kwargs.get("rotation", np.zeros(3))
+        self.new_r = np.zeros(2)
+
+        if 'target' in kwargs:
+            self.r = np.array([np.cos(φ), np.sin(φ)]) * radius + kwargs['target'].r
+            self.dr = dr + kwargs['target'].dr
+        else:
+            self.r = np.array([np.cos(φ), np.sin(φ)]) * radius
+            self.dr = dr
+        self.trace = [copy.copy(self.r)]
+        self.vtrace = [copy.copy(self.dr)]
+
+    def step(self):
+        self.r += self.new_r
+        self.trace.append(copy.copy(self.r))
+        self.vtrace.append(copy.copy(self.dr))
+
+
+class PType(CelestialBody):
+    def __init__(self, rad, φ, dr, **kwargs):
+        super().__init__(rad, φ, dr, **kwargs)
+        self.mass = kwargs.get("mass", 1)
+
+    def F(self, r, dr):
+        """Planet creates weak enough gravitation field.
+        Returns acceleration."""
+        n = np.linalg.norm(r)
+        return -r * self.mass / n ** 2
+
+
+class SType(CelestialBody):
+    def __init__(self, rad, φ, dr, **kwargs):
+        super().__init__(rad, φ, dr, **kwargs)
+        self.mass = kwargs.get("mass", 1)
+        self.gravrad = self.mass / 9e16  # 9e16 = speed of light
+
+    def F(self, r, dr):
+        """Star creates weak strong gravitation field.
+        Returns acceleration"""
+        n = np.linalg.norm(r)
+        return -r * self.mass * np.exp(self.gravrad / n) / n ** 2
+
+
+class Universe():
+    def __init__(self, G=6.674e-11):
+        """G - gravitation constant"""
+        self.G = G
+        self.objects = []
+        self.delobjects = []
+
+    def append(self, obj: CelestialBody):
+        self.objects.append(obj)
+
+    def step(self):
+        for a in self.objects:
+            a.new_r = np.zeros(2)
+            force = np.array([0, 0], dtype=float)
+            for b in self.objects:
+                if a != b:
+                    force += b.F(a.r - b.r, a.dr)
+            a.dr += force * simulation.dt
+            a.new_r += a.dr * simulation.dt
+        for a in self.objects:
+            a.step()
+
+
+u = Universe()
+
+star_a = SType(0, 0, np.array([0, 0], dtype=float), )
+star_a.mass = 10000
+star_b = SType(-7, 0, np.array([0, -70], dtype=float), )
+star_c = SType(7, 0, np.array([0, 70], dtype=float), )
+planet_0 = PType(20, 0, np.array([0, 170], dtype=float), target=star_a)
+planet_0.mass = 10
+planet_1 = PType(-20, 0, np.array([0, 70], dtype=float), target=star_a)
+planet_1.mass = 10
+
+u.append(star_a)
+u.append(star_b)
+u.append(star_c)
+u.append(planet_0)
+u.append(planet_1)
+
+
+def kepler(r, dr):
+    ds = np.cross(r, dr) / 2
+    axis = range(len(ds))
+    plt.plot(axis, ds)
+    plt.show()
+
+
+lines = tuple([])
+for n in range(len(u.objects)):
+    line, = ax.plot([], [], lw=3)
+    lines += tuple([line])
+
+
+def init():
+    for n in range(len(u.objects)):
+        lines[n].set_data([], [])
+    return lines
+
+
+for i in range(TAIL):
+    u.step()
+
+
+def animate(t):
+    u.step()
+    n = 0
+
+    for o in u.objects:
+        d = tuple(map(list, zip(*o.trace[-TAIL:])))
+        lines[n].set_data(d)
+
+        n += 1
+    return lines
+
+
+import matplotlib.backend_bases
+
+
+def onclick(event: matplotlib.backend_bases.KeyEvent):
+    # print(event.key)
+    if (event.key == '1'):
+        simulation.dt *= 1.5
+    elif (event.key == '2'):
+        simulation.dt /= 1.5
+    elif (event.key == ' '):
+        if not simulation.pause:
+            anim.pause()
+            simulation.pause = 1
+        else:
+            anim.resume()
+            simulation.pause = 0
+
+
+cid = fig.canvas.mpl_connect('key_press_event', onclick)
+
+anim = FuncAnimation(fig, animate, init_func=init, frames=300, interval=5, blit=True)
+
+plt.show()
+
+for o in u.objects:
+    plt.plot(*tuple(map(list, zip(*o.trace))))
+plt.gca().set_aspect('equal')
+plt.show()
+
+kepler(planet_0.trace, planet_0.vtrace)
+kepler(planet_1.trace, planet_1.vtrace)
