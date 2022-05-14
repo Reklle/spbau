5bd11b287a84c23e0e1b620befa748bc5c495f00
2, 8, 20, 28, 50, 82, 126 <53151326+Reklle@users.noreply.github.com>
2022-05-14 21:30:09 +0300

    v 0.1

---
+++ data sciense on R/sympy.R
@@ -0,0 +1,22 @@
+# install.packages("caracas")
+library(caracas)
+#caracas::install_sympy()
+
+
+#########################
+#         Note          #
+#########################
+#  der(f)  --   derivative
+#  der(f, c(x, y))
+#  subs    --   substitution
+#  eval    --   evaluate
+#  solve_sys(eq, x)
+
+
+x <- symbol('x')
+eq <- 2*x^2 - x
+eq <- der(eq, x)
+tex(eq)
+
+y <- 10
+subs(eq, x, y)
